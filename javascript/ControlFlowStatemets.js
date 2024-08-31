8e4586785cbbfc31d46739853dd9b95451bb1349
Ubuntu <labuser@ubuntu2204.bd4i0odyyk5exmyvfiorwzw0ab.rx.internal.cloudapp.net>
2024-08-31 15:09:19 +0000

    added javascript examples

--- javascript/ControlFlowStatemets.js
+++ javascript/ControlFlowStatemets.js
@@ -33,6 +33,60 @@ switch(fruit){
     console.log("It's something else");
 }
 
+// Looping- for, while, do.....while, for....in, for...of
 
+console.log("For Loop...")
+for(let i = 1; i <= 5; i++){
+    console.log(i);
+}
 
+console.log("While Loop...")
+let counter = 3;
+while(counter > 0){
+    console.log(counter);
+    counter--;
+}
 
+console.log("Do...While Loop...")
+let doWhileCounter = 2;
+
+do{
+    console.log(doWhileCounter);
+    doWhileCounter--;
+}
+while(doWhileCounter > 0);
+
+console.log("For...In Loop- Object Iteration");
+let person = {
+    name: "John",
+    age: 30,
+    job: "Developer"
+};
+
+for(let key in person){
+    console.log(key + " : " + person[key]);
+}
+
+console.log("For...Of Loop...");
+
+let numbers = [1, 2, 3, 4, 5];
+for(let num of numbers){
+    console.log(num);
+}
+
+// Break and Continue Statments
+console.log("Break Statement:")
+for (let i = 1; i <= 5; i++){
+    if(i === 3){
+        break;
+    }
+    console.log(i);
+}
+
+console.log("Continue Statement:")
+for (let i = 1; i <= 5; i++){
+    if(i === 3){
+        continue;
+    }
+    console.log(i);
+}
